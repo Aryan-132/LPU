cdeb33a3093b731901819eafd7686d96a5fac219
Aryan-132 <mr.aryanraj110@gmail.com>
2024-08-26 10:06:52 +0530

    hgdy

--- Python Programming (INT-108)/Class 1/SI.py
+++ Python Programming (INT-108)/Class 1/SI.py
@@ -8,6 +8,17 @@ p=float(input("Enter Principal Value: "))
 t=float(input("Enter the Time Periods:"))
 r=float(input("Enter the Interest rate: "))
 
+if SI<=100:
+    
+    tax=0
+elif SI<=1000 and SI > 100:
+    tax=0.05*SI
+else:
+     tax=0.1*SI
+print(tax)
+    
+
+
 print("The Simple Interest is :",SI(p,t,r))
 
     
---
+++ Python Programming (INT-108)/Class 4/main.py
@@ -0,0 +1,9 @@
+a=65.3
+
+print(f"{a:.2f}")
+print(f"{a:10.2f}")
+print(f"{a:3.2f}")
+
+b=65.3
+print(round(b,3))
+      
