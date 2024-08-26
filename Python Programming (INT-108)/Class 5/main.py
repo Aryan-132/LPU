74d159cd460c94c2da4ce44836e509fd72ae2ec6
Aryan-132 <mr.aryanraj110@gmail.com>
2024-08-26 10:24:29 +0530

    List

---
+++ Python Programming (INT-108)/Class 5/main.py
@@ -0,0 +1,14 @@
+list1=["Hello","Welcome","to",["a","b",5,6,8 ],23,26,48 ]
+
+print (list1[3])
+print(list1[3][2])
+print("a" in list1)
+print("a" in list1[3])
+print(list1[3][4])
+print("Aryan" not in list1)
+print("Aryan" in list1)
+
+print("Hello" in list1)
+print(26 in list1)
+print("To" in list1)
+print("hello" not in list1)
